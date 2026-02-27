062cdeadacee66a5ff5971c92582b15e3d1b61eb
Zovin Anggleas <anggleaszovin@gmail.com>
2026-02-27 16:13:09 +1100

    finished Task 1 and Task 2

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import HTTPError from 'http-errors';
 
 // ==== Type Definitions, feel free to add or modify ==========================
 interface cookbookEntry {
@@ -17,6 +18,11 @@ interface recipe extends cookbookEntry {
 
 interface ingredient extends cookbookEntry {
   cookTime: number;
+}
+
+interface summary extends cookbookEntry {
+  cookTime: number;
+  requiredItems: requiredItem[];
 }
 
 // =============================================================================
@@ -45,24 +51,58 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  recipeName = recipeName
+    .replace(/[-_]/g, " ", )  // replaces - and _ to whitespace
+    .toLowerCase()
+    .replace(/[^a-z ]/g, "")  // removes all symbols
+    .trim()
+    .replace(/\s+/g, " ")     // remove duplicate whitespace
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1)) // capitalise first letter
+    .join(' ');
+
+  if (!recipeName.length) return null;
+  return recipeName;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const input: recipe | ingredient = req.body;
 
+  res.status(200).send(add_entry(input));
 });
+
+// adds new entry to cookbook
+const add_entry = (input: recipe | ingredient): void => {
+  if (!(input.type === "ingredient" || input.type === "recipe")) {
+    throw(HTTPError(400));
+  } else if (cookbook.find((i) => i.name === input.name)) {
+    throw(HTTPError(400));
+  }
+
+  if (input.type === "recipe") {
+    const itemNames = (input as recipe).requiredItems.map((i) => i.name);
+    if (new Set(itemNames).size !== itemNames.length) {
+      throw(HTTPError(400));
+    }
+
+    cookbook.push(input as recipe);
+  } else {
+    if ((input as ingredient).cookTime < 0) {
+      throw (HTTPError(400));
+    }
+
+    cookbook.push(input as ingredient);
+  }
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const input : string = req.query.name;
 
+  res.status(200).send(get_summary(input));
 });
 
 // =============================================================================
